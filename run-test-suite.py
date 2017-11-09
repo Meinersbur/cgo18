d036c526d568d06dabff1380c5eeed114f57c741
Michael Kruse <llvm@meinersbur.de>
2017-11-09 18:28:02 +0100

    [r317808/75ccfc] -v --suite-exec-threads=1 --revision=r317808 --llvm-revision=5be95c616464685ae999906ad030accfd0774bf0 --clang-revision=5140a709d303679fce3218d238fd4ee5043ae851 --libcxxabi-revision=05ba3281482304ae8de31123a594972a495da06d --libcxx-revision=eada02443aaa17a44ef6c8401f042eb48f1a3fcc --suite-revision=3329a21363eef1ffbe488ced0342ab594e193004 --llvm-build-cmakedefs=LLVM_ENABLE_ASSERTIONS=ON --test-only=Performance/Polybench-421 --test-only=Performance/Polybench-421-boost --suite-build-flags=-ffast-math --suite-build-flags=-march=native --suite-build-flags=-DEXTRALARGE_DATASET --suite-build-flags=-fno-exceptions --suite-build-mllvm=-polly-only-func=kernel_ --suite-build-mllvm=-polly-optree-max-ops=0 --suite-build-mllvm=-inline-threshold=4096 --suite-build-mllvm=-polly-optree-normalize-phi --suite-build-mllvm=-polly-position=early --suite-build-mllvm=-polly-enable-simplify=0 --suite-build-mllvm=-polly-enable-optree=0 --suite-build-mllvm=-polly-enable-delicm=0 --polly --stats --multisample=5
    Job create using command:
    /c/Users/Meinersbur/src/gittool/gittool.py makejob A30_polly_early --suite-build-flags=-ffast-math --suite-build-flags=-march=native --llvm-build-cmakedefs=LLVM_ENABLE_ASSERTIONS=ON --stats -v --no-gitignore --exec-threads 1 --multisample 5 --test-only Performance/Polybench-421 --test-only Performance/Polybench-421-boost --mllvm=-polly-only-func=kernel_ --mllvm=-polly-optree-max-ops=0 --suite-build-flags=-DEXTRALARGE_DATASET --mllvm=-inline-threshold=4096 --cppflags=-fno-exceptions --mllvm=-polly-optree-normalize-phi --polly --suite-build-mllvm=-polly-position=early --suite-build-mllvm=-polly-enable-simplify=0 --suite-build-mllvm=-polly-enable-optree=0 --suite-build-mllvm=-polly-enable-delicm=0
    
    Exec: python3 ./run-test-suite.py -v --suite-exec-threads=1 --revision=r317808 --llvm-revision=5be95c616464685ae999906ad030accfd0774bf0 --clang-revision=5140a709d303679fce3218d238fd4ee5043ae851 --libcxxabi-revision=05ba3281482304ae8de31123a594972a495da06d --libcxx-revision=eada02443aaa17a44ef6c8401f042eb48f1a3fcc --suite-revision=3329a21363eef1ffbe488ced0342ab594e193004 --llvm-build-cmakedefs=LLVM_ENABLE_ASSERTIONS=ON --test-only=Performance/Polybench-421 --test-only=Performance/Polybench-421-boost --suite-build-flags=-ffast-math --suite-build-flags=-march=native --suite-build-flags=-DEXTRALARGE_DATASET --suite-build-flags=-fno-exceptions --suite-build-mllvm=-polly-only-func=kernel_ --suite-build-mllvm=-polly-optree-max-ops=0 --suite-build-mllvm=-inline-threshold=4096 --suite-build-mllvm=-polly-optree-normalize-phi --suite-build-mllvm=-polly-position=early --suite-build-mllvm=-polly-enable-simplify=0 --suite-build-mllvm=-polly-enable-optree=0 --suite-build-mllvm=-polly-enable-delicm=0 --polly --stats --multisample=5

---
+++ run-test-suite.py
@@ -0,0 +1,108 @@
+#! /usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import os
+import multiprocessing
+import argparse
+import configparser
+import shlex
+import shutil
+import sys
+from distutils.util import strtobool
+from gittool import Repository2, Commit, Branch, Remote, invoke, checkout_sub, llvm, REVISION, revToStr, RuntestConfig, parseRev, shjoin
+import gittool
+import platform
+
+
+def try_display(filename):
+    try:
+        with open(filename, 'r') as f:
+            print("\n{filename}:".format(filename=filename))
+            print(f.read())
+    except:
+        print("Could not access {filename}".format(filename=filename),file=sys.stderr)
+
+
+
+
+
+def main(argv=sys.argv[1:]):
+    hostname = platform.node()
+
+    print("Running on:", hostname)
+
+    parser = argparse.ArgumentParser()
+    gittool.RuntestConfig.add_runtest_arguments(parser)
+
+    print("job config:" , shjoin(argv))
+    args = parser.parse_args(args=argv)
+    config = RuntestConfig.from_cmdargs(args)
+    print("Parsed config:", config.get_cmdline())
+    #config.llvm_source_dir = args.llvm_source_dir
+
+    workdir = '.'
+    workdir = os.path.abspath(workdir)
+
+
+    persistentdir = os.environ['persistentdir']
+    persistentdir = os.path.abspath(persistentdir)
+
+
+
+    default_config = os.environ.get('RUNTEST_CONFIG_DEFAULT','')
+    print("default config:" , default_config)
+    default_config = shlex.split(default_config)
+    default_config,_ = parser.parse_known_args(default_config)
+    if _:
+        print("Warning: unknown default args:",_)
+    default_config = RuntestConfig.from_cmdargs(default_config)
+
+    override_config = os.environ.get('RUNTEST_CONFIG_OVERRIDE','')
+    print("override config:" , override_config)
+    override_config = shlex.split(override_config)
+    override_config,_ = parser.parse_known_args(override_config)
+    if _:
+        print("Warning: unknown default args:",_)
+    override_config = RuntestConfig.from_cmdargs(override_config)
+
+    config.merge(default_config)
+    override_config.merge(config)
+    config = override_config
+
+    print("effective config:", config.get_cmdline())
+
+
+    if config.polly_source_dir is None:
+        config.polly_source_dir = workdir
+    if config.llvm_source_dir is None:
+        config.llvm_source_dir = os.path.join(persistentdir, 'llvm')
+    if config.llvm_build_dir is None:
+        config.llvm_build_dir = os.path.join(persistentdir, 'llvm-build')
+    if config.suite_build_dir is None:
+        config.suite_build_dir = os.path.join(workdir, 'run-test-suite')
+
+
+    use_perf = config.suite_build_useperf
+    has_perf = False
+    try:
+        invoke('perf', 'stat', 'ls')
+        has_perf = True
+    except:
+        pass
+    print("Has_perf result is {has_perf} (use_perf={use_perf})".format(has_perf=has_perf,use_perf=use_perf))
+
+    try_display('/proc/cpuinfo')
+    try_display('/proc/meminfo')
+    try_display('/proc/loadavg')
+    try_display('/proc/uptime')
+    try_display('/proc/version')
+
+
+    config.llvm_checkout = True
+    config.llvm_configure = True
+
+    gittool.runtest(config,print_logs=True,enable_svn=False)
+
+
+if __name__ == '__main__':
+    main()
